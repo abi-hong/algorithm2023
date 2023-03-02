f869a568538f60e07bcd9b7aa4463d7f297d3c22
abi-hong <flffjakfwm20@gmail.com>
2023-03-02 22:24:23 +0900

    bottom-up 방식

---
+++ Chapter8/FibonacciSequence3.java
@@ -0,0 +1,16 @@
+package Chapter8;
+
+public class FibonacciSequence3 {
+    public static long[] d = new long[100];
+
+    public static void main(String[] args) {
+        d[1] = 1;
+        d[2] = 1;
+        int n = 50;
+
+        for (int i = 3; i <= n; i++) {
+            d[i] = d[i-1] + d[i-2];
+        }
+        System.out.println(d[n]);
+    }
+}
