070a15330aa3464ddb9b593f7b9b7c35f64dee55
abi-hong <flffjakfwm20@gmail.com>
2023-08-15 22:24:41 +0900

    완전 제곱수

---
+++ 2023_ICPC/day4/6131.py
@@ -0,0 +1,12 @@
+#A^2 = B^2 + n
+
+N = int(input())
+
+cnt = 0
+for i in range(1, N):
+    if i**2 - (i-1)**2 > N:
+        break
+    for j in range(1, i+1):
+        if i**2 - j**2 == N:
+            cnt += 1
+print(cnt)
