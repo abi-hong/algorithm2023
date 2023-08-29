f2893ddf7656f28bb9467ba1cb9a526aa90ddcbf
abi-hong <flffjakfwm20@gmail.com>
2023-08-29 23:22:18 +0900

    최소 힙

---
+++ 2023_ICPC/day8/1927.py
@@ -0,0 +1,21 @@
+import heapq
+import sys
+input =  sys.stdin.readline
+
+# 최소힙
+# 1. 배열에 자연수 x를 넣는다.
+# 2. 배열에서 가장 작은 값을 출력하고, 그 값을 배열에서 제거한다.
+n = int(input())
+
+heap = []
+for _ in range(n):
+    num = int(input())
+    if (num == 0):
+        # 가장 작은 값 출력하고 제거
+        if (len(heap) == 0):
+            print(0)
+        else:
+            print(heapq.heappop(heap))
+    else:
+        # 배열에 x라는 값을 넣는 연산
+        heapq.heappush(heap, num)
